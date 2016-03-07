df82d18fb33ecf297c46cbaa4a0377de44e89ca6
蜗壳 <treewalker.fu@gmail.com>
2016-03-07 21:06:31 +0800

    初始版本提交

---
+++ Design/Design/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Design
+//
+//  Created by tree on 16/3/7.
+//  Copyright © 2016年 tree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Design/Design/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Design
+//
+//  Created by tree on 16/3/7.
+//  Copyright © 2016年 tree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
