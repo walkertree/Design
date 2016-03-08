7d77ca9f07bbb5c8442ee2cfd9edc047cb816332
蜗壳 <treewalker.fu@gmail.com>
2016-03-08 18:06:38 +0800

    迭代器模式提交记录下

--- Design/Design/ViewController.m
+++ Design/Design/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
+#define Number_Array (@[@"0",@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9"])
+
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *itemDisplay;
 
 @end
 
@@ -16,12 +19,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)enumeratorPress:(id)sender {
+    self.itemDisplay.text = @"";
+    NSEnumerator *enumerator = [Number_Array objectEnumerator];
+    NSString *item;
+    while (item = [enumerator nextObject]) {
+        
+    }
+    
+}
+
+- (IBAction)blockPress:(id)sender {
+}
+
+- (IBAction)quickPress:(id)sender {
+}
+
+- (IBAction)innerPress:(id)sender {
 }
 
 @end
